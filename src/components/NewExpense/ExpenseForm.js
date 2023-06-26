36e970c4c3c43d27c45bf349b8fc9fa20d4975ba
Navdeep <rathorenavdeep110@gmail.com>
2023-06-26 09:26:16 +0530

    Adding two way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -42,31 +42,55 @@ const [enteredDate, setEnteredDate] = useState('');
     // });
    };
 
-  const inputChangeHandler = (identifier, value) => {
-    if(identifier === 'title'){
-        setEnteredTitle(value);
-    } else if(identifier === 'date'){
-        setEnteredDate(value);
-    } else {
-        setEnteredAmount(value);
-    }
-  };
+//   const inputChangeHandler = (identifier, value) => {
+//     if(identifier === 'title'){
+//         setEnteredTitle(value);
+//     } else if(identifier === 'date'){
+//         setEnteredDate(value);
+//     } else {
+//         setEnteredAmount(value);
+//     }
+//   };
    
-
+const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+    };
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+};
     return (
-    <form>
+    <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className='new-expense__control'>
                <label>Title</label>
-               <input type='text' onChange={(event) => titleChangeHandler} />
+               <input type='text' 
+                value={enteredTitle}
+               onChange={titleChangeHandler} 
+               />
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler} />
+                <input type='number' 
+                min="0.01" 
+                step="0.01" 
+                value={enteredAmount}
+                onChange={amountChangeHandler} 
+                />
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type='date' min="2023-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+                <input type='date' 
+                min="2000-01-01" 
+                max="2023-12-31" 
+                value={enteredDate}
+                onChange={dateChangeHandler} 
+                />
             </div>
         </div>
         <div className='new-expense__actions'>
