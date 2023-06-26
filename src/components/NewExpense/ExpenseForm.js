419a908243c2f420e2620596742904e7ec01ec5f
Navdeep <rathorenavdeep110@gmail.com>
2023-06-26 08:53:37 +0530

    multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,41 +3,54 @@ import React, {useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-// const [enteredTitle, setEnteredTitle] = useState('');
-// const [enteredAmount, setEnteredAmount] = useState('');
-// const [enteredDate, setEnteredDate] = useState('');
+const [enteredTitle, setEnteredTitle] = useState('');
+const [enteredAmount, setEnteredAmount] = useState('');
+const [enteredDate, setEnteredDate] = useState('');
 
-const [userInput, setUserInput] = useState({
-    enteredTitle: '', 
-    enteredAmount: '',
-    enteredDate: ''
-});
+// const [userInput, setUserInput] = useState({
+//     enteredTitle: '', 
+//     enteredAmount: '',
+//     enteredDate: ''
+// });
 
    const titleChangeHandler = (event) => {
-    //   setEnteredTitle(event.target.value);
-    setUserInput({
-        ...userInput,
-        enteredTitle: event.target.value,
-    });
+      setEnteredTitle(event.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredTitle: event.target.value,
+    // });
+
+    //Latest State Snapshot
+//     setUserInput((prevState) => {
+//         return { ...prevState, enteredTitle: event.target.value};
+//     });
    };
 
    const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-        ...userInput,
-        enteredAmount: event.target.value,
-    });
+    setEnteredAmount(event.target.value);
+//     setUserInput({
+//         ...userInput,
+//         enteredAmount: event.target.value,
+//     });
    };
 
    const dateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
-    setUserInput({
-        ...userInput,
-        enteredDate: event.target.value,
-    });
-   }
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredDate: event.target.value,
+    // });
+   };
 
-  
+  const inputChangeHandler = (identifier, value) => {
+    if(identifier === 'title'){
+        setEnteredTitle(value);
+    } else if(identifier === 'date'){
+        setEnteredDate(value);
+    } else {
+        setEnteredAmount(value);
+    }
+  };
    
 
     return (
@@ -45,7 +58,7 @@ const [userInput, setUserInput] = useState({
         <div className="new-expense__controls">
             <div className='new-expense__control'>
                <label>Title</label>
-               <input type='text' onChange={titleChangeHandler} />
+               <input type='text' onChange={(event) => titleChangeHandler} />
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
